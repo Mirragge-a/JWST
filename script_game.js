c6c1b5f65325cc61fd3f999522fc37804ee935b4
Mirragge-a <artemshakhrai@ukr.net>
2023-04-03 11:19:49 +0300

    v1 03.04

---
+++ script_game.js
@@ -0,0 +1,9 @@
+document.addEventListener('keydown', function(event) {
+    if (event.code == 'KeyA') {
+        document.getElementById("jwst").style.marginleft = "100px";
+  }});
+
+//   document.addEventListener('keydown', function(event) {
+//     if (event.code == 'KeyD') {
+//         document.getElementById("jwst").style.paddingleft = "650px";
+// }});
