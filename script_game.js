11133c8228995263fb2f21ef7fbc13bc97cebe68
Mirragge-a <artemshakhrai@ukr.net>
2023-04-04 09:48:52 +0300

    v1 04.04

--- script_game.js
+++ script_game.js
@@ -1,7 +1,27 @@
-document.addEventListener('keydown', function(event) {
-    if (event.code == 'KeyA') {
-        document.getElementById("jwst").style.marginleft = "100px";
-  }});
+var left=0
+if(left < 90) {
+  document.addEventListener('keydown', function(event) {
+  if ((event.code == 'KeyA')) {
+      left -= 5
+      if(left > 4) {
+        document.getElementById("jwst").style.marginLeft = left + "%";
+      } else {
+        left = 5
+      }
+  } else if (event.code == 'KeyD') {
+      left += 5
+      if(left < 86) {
+        document.getElementById("jwst").style.marginLeft = left + "%";
+      } else {
+        left = 85
+      }
+      
+    }
+})
+} else if(left = 1500){
+  document.getElementById("jwst").style.marginLeft = "1499px";
+}
+
 
 //   document.addEventListener('keydown', function(event) {
 //     if (event.code == 'KeyD') {
