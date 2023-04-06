262e636ce7782974227a4a9f89c5b63bcc52d61b
Mirragge-a <artemshakhrai@ukr.net>
2023-04-06 17:01:17 +0300

    v1 06.04

--- script_game.js
+++ script_game.js
@@ -1,29 +1,30 @@
-var left=0
-if(left < 90) {
-  document.addEventListener('keydown', function(event) {
-  if ((event.code == 'KeyA')) {
-      left -= 5
-      if(left > 4) {
-        document.getElementById("jwst").style.marginLeft = left + "%";
-      } else {
-        left = 5
+
+function startFunction() {
+  alert('Гра розпочата!')
+  // enemy
+  
+  // player control
+  var left=45
+  if(left < 90) {
+    document.addEventListener('keydown', function(event) {
+    if ((event.code == 'KeyA')) {
+        left -= 5
+        if(left > 4) {
+          document.getElementById("jwst").style.marginLeft = left + "%";
+        } else {
+          left = 5
+        }
+    } else if (event.code == 'KeyD') {
+        left += 5
+        if(left < 86) {
+          document.getElementById("jwst").style.marginLeft = left + "%";
+        } else {
+          left = 85
+        }
+        
       }
-  } else if (event.code == 'KeyD') {
-      left += 5
-      if(left < 86) {
-        document.getElementById("jwst").style.marginLeft = left + "%";
-      } else {
-        left = 85
-      }
-      
-    }
-})
-} else if(left = 1500){
-  document.getElementById("jwst").style.marginLeft = "1499px";
+  })
+  } else if(left = 1500){
+    document.getElementById("jwst").style.marginLeft = "1499px";
+  }
 }
-
-
-//   document.addEventListener('keydown', function(event) {
-//     if (event.code == 'KeyD') {
-//         document.getElementById("jwst").style.paddingleft = "650px";
-// }});
